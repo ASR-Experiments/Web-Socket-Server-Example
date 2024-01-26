86ca073c21e9185e606e272f5d7563b513d0c5e7
Arvind Singh Rawat <39412688+ArvindSinghRawat@users.noreply.github.com>
2024-01-26 20:14:10 +0530

    Done Refactoring - Added Java Docs - Changed logs from Info to Debug   - To reduce default chatter - Moved web-socket path to custom property

--- src/main/java/com/asr/personal/web/socket/server/config/WebSocketConfig.java
+++ src/main/java/com/asr/personal/web/socket/server/config/WebSocketConfig.java
@@ -2,6 +2,7 @@ package com.asr.personal.web.socket.server.config;
 
 import com.asr.personal.web.socket.server.handler.MessageHandler;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
@@ -14,10 +15,16 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 @RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketConfigurer {
 
+    @Value("${my.web.socket.path}")
+    private String path;
+
     private final MessageHandler messageHandler;
 
+    /**
+     * Gets the custom handler and register it to provided endpoint
+     */
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(messageHandler, "/web-socket");
+        registry.addHandler(messageHandler, path);
     }
 }
--- src/main/java/com/asr/personal/web/socket/server/handler/MessageHandler.java
+++ src/main/java/com/asr/personal/web/socket/server/handler/MessageHandler.java
@@ -16,30 +16,35 @@ import java.time.format.DateTimeFormatter;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * Custom message handler service to handle Text messages
+ */
 @Slf4j
 @Service
 public class MessageHandler extends TextWebSocketHandler {
-    private final Set<WebSocketSession> SESSIONS = new HashSet<>();
+    private final Set<WebSocketSession> activeSessionsList = new HashSet<>();
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         super.afterConnectionEstablished(session);
-        SESSIONS.add(session);
+        log.debug("Session added with ID {}", session.getId());
+        activeSessionsList.add(session);
     }
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
         super.afterConnectionClosed(session, status);
-        SESSIONS.remove(session);
+        log.debug("Session removed with ID {}", session.getId());
+        activeSessionsList.remove(session);
     }
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) {
         String request = message.getPayload();
-        log.info("Server received: {}", request);
+        log.debug("Server received: {}", request);
 
         String response = String.format("Response from Server to '%s'", HtmlUtils.htmlEscape(request));
-        log.info("Server Sends: {}", response);
+        log.debug("Server Sends: {}", response);
 
         try {
             session.sendMessage(new TextMessage(response));
@@ -50,10 +55,10 @@ public class MessageHandler extends TextWebSocketHandler {
 
     @Scheduled(fixedRate = 10000)
     void sendPeriodicMessages() throws IOException {
-        for (WebSocketSession session : SESSIONS) {
+        for (WebSocketSession session : activeSessionsList) {
             // For each session, send a message
             String message = "Server's periodic broadcast message at " + OffsetDateTime.now().format(DateTimeFormatter.RFC_1123_DATE_TIME);
-            log.info("Server sends : {}", message);
+            log.debug("Server sends : {}", message);
             session.sendMessage(new TextMessage(message));
         }
     }
