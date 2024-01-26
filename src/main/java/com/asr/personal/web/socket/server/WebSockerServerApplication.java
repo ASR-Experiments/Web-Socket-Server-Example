bd5d7f35349f4511cd1fd881f804fcc43b20bee1
Arvind Singh Rawat <39412688+ArvindSinghRawat@users.noreply.github.com>
2024-01-26 19:00:21 +0530

    Boilerplate setup

---
+++ src/main/java/com/asr/personal/web/socket/server/WebSockerServerApplication.java
@@ -0,0 +1,13 @@
+package com.asr.personal.web.socket.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSockerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSockerServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asr/personal/web/socket/server/WebSockerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asr.personal.web.socket.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSockerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
