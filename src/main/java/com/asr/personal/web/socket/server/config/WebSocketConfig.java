0b4a6f2f63263e2975b649a4a96f2ab2463a4f8b
Arvind Singh Rawat <39412688+ArvindSinghRawat@users.noreply.github.com>
2024-01-26 20:06:47 +0530

    Added server's side periodic messages

--- src/main/java/com/asr/personal/web/socket/server/config/WebSocketConfig.java
+++ src/main/java/com/asr/personal/web/socket/server/config/WebSocketConfig.java
@@ -3,12 +3,14 @@ package com.asr.personal.web.socket.server.config;
 import com.asr.personal.web.socket.server.handler.MessageHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @Configuration
 @EnableWebSocket
+@EnableScheduling
 @RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketConfigurer {
 
--- src/main/java/com/asr/personal/web/socket/server/handler/MessageHandler.java
+++ src/main/java/com/asr/personal/web/socket/server/handler/MessageHandler.java
@@ -2,6 +2,7 @@ package com.asr.personal.web.socket.server.handler;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.MessagingException;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
@@ -10,6 +11,8 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 import org.springframework.web.util.HtmlUtils;
 
 import java.io.IOException;
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -42,6 +45,16 @@ public class MessageHandler extends TextWebSocketHandler {
             session.sendMessage(new TextMessage(response));
         } catch (IOException e) {
             throw new MessagingException("Error while sending message", e);
+        }
+    }
+
+    @Scheduled(fixedRate = 10000)
+    void sendPeriodicMessages() throws IOException {
+        for (WebSocketSession session : SESSIONS) {
+            // For each session, send a message
+            String message = "Server's periodic broadcast message at " + OffsetDateTime.now().format(DateTimeFormatter.RFC_1123_DATE_TIME);
+            log.info("Server sends : {}", message);
+            session.sendMessage(new TextMessage(message));
         }
     }
 }
